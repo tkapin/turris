a02b5c848024b13dec14f16eaa5bc22822161cf5
Tomas Kapin <tomas.kapin@microsoft.com>
2025-09-26 13:12:57 +0200

    Get configs script to redact secrets

--- configs/Get-Config.ps1
+++ configs/Get-Config.ps1
@@ -1,1 +1,1 @@
-(ssh root@turris "uci show") -replace "(username|password)='(.*)'","`$1='***'"
+(ssh root@turris "uci show") -replace "^(network\.wan\.username|network\.wan\.password|wireless\.default_radio[\d]\.key)='(.*)'$","`$1='***'"
